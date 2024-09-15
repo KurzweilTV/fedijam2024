7a4cac922a6c39cca565865c5b147e0ed84200b1
Mike Barr <m.barr@mac.com>
2024-09-15 10:55:50 +0200

    start of level design, including level class

---
+++ scenes/level_base.gd
@@ -0,0 +1,15 @@
+class_name BaseLevel
+extends Node2D
+
+@onready var scenes: TileMapLayer = $Layers/Scenes
+@onready var fader: AnimationPlayer = %AnimationPlayer
+
+var default_item_spawns
+
+func _ready() -> void:
+	default_item_spawns = scenes.tile_map_data
+	Messages.spawn_player.connect(respawn_items)
+	fader.play("fade_animation")
+
+func respawn_items() -> void:
+	scenes.tile_map_data = default_item_spawns
--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var menu_animator: AnimationPlayer = $OptionsScreen/AnimationPlayer
 @onready var click: AudioStreamPlayer = $MenuSounds/Click
 @onready var hover: AudioStreamPlayer = $MenuSounds/Hover
+@onready var fader: AnimationPlayer = $Fader/AnimationPlayer
 
 @export var first_level:PackedScene
 
@@ -10,6 +11,8 @@ func play_hover():
 	hover.play()
 
 func _on_start_button_pressed() -> void:
+	fader.play_backwards("fade_animation")
+	await fader.animation_finished
 	get_tree().change_scene_to_packed(first_level)
 	
 func _on_options_button_pressed() -> void:
